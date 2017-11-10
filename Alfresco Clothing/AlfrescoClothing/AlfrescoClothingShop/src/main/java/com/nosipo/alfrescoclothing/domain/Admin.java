0241e47795952893147f59dc05829657d07ce510
nosipo <nndumani@gmail.com>
2017-11-10 04:26:47 -0800

    Add files via upload

---
+++ Alfresco Clothing/AlfrescoClothing/AlfrescoClothingShop/src/main/java/com/nosipo/alfrescoclothing/domain/Admin.java
@@ -0,0 +1,61 @@
+package com.nosipo.alfrescoclothing.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    private String id;
+    private String name;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    private Admin() {
+    }
+
+    public Admin(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.password = builder.password;
+    }
+
+    public static class Builder{
+        private String id;
+        private String name;
+        private String password;
+
+        public Builder id(String value){
+            this.id = value;
+            return this;
+        }
+
+        public Builder name(String value){
+            this.name = value;
+            return this;
+        }
+
+        public Builder password(String value){
+            this.password = value;
+            return this;
+        }
+
+        public Admin build(){
+
+            return new Admin(this);
+        }
+
+    }
+}
